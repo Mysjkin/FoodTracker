9098e4a232bf0a4c2da9d79ab695cff6c0cae001
Iaroslav Golovanov <iarogolo@gmail.com>
2020-08-29 00:05:41 +0200

    added route constants

--- App/foodtracker/src/constants/routes.js
+++ App/foodtracker/src/constants/routes.js
@@ -3,3 +3,5 @@ export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/login';
 export const PROFILE = '/profile';
+export const SEARCH = '/search';
+export const FOOD_SELECTION = '/foods/:id';
